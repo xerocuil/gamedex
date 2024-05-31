27e14a225131c2ed312b41fee06d528dcc90b572
xerocuil <tbrodhead@mm.st>
2024-05-30 23:10:00 -0400

    Added classes to `library.admin`

--- src/library/admin.py
+++ src/library/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Collection, Game, Genre, Platform, Tag
+
+
+def mark_as_archived(modeladmin, request, queryset):
+    queryset.update(archived=True)
+
+
+mark_as_archived.short_description = "Archived"
+
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'developer', 'publisher', 'platform', 'archived')
+    search_fields = (
+        'title',
+        'path',
+        'genre__name',
+        'developer',
+        'publisher',
+        'platform__name',
+    )
+    actions = [mark_as_archived]
+
+
+admin.site.register(Collection)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Genre)
+admin.site.register(Platform)
+admin.site.register(Tag)
