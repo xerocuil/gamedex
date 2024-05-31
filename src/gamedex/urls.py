fa393dd389fa0b0d44423210e008418057601daa
xerocuil <tbrodhead@mm.st>
2024-05-30 23:04:28 -0400

    Added static paths to `gamedex.urls`

--- src/gamedex/urls.py
+++ src/gamedex/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('library.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
