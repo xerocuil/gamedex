5573fdebeb7ddae544adb43e540e4e82e7e166a1
xerocuil <tbrodhead@mm.st>
2024-06-15 20:49:52 -0400

    Added export_installed_games to `extensions.lutris`

--- src/extensions/lutris.py
+++ src/extensions/lutris.py
@@ -65,3 +65,37 @@ def import_data():
 
     with open(lutris_missing, 'w') as f:
         json.dump(not_in_db, f, indent=4)
+
+
+def export_installed_games():
+    lutris_dir = os.path.join(
+        os.path.expanduser('~'),
+        '.gamedex', 'sys', 'json', 'lutris')
+    lutris_json = os.path.join(lutris_dir, 'games.json')
+    lt_query = 'SELECT\
+        directory, installed, lastplayed, name, platform, playtime, service, slug\
+        FROM games WHERE installed = 1;'
+    LT_CURSOR.execute(lt_query)
+    columns = [description[0] for description in LT_CURSOR.description]
+    results = []
+
+    for row in LT_CURSOR.fetchall():
+        result = dict(zip(columns, row))
+        results.append(result)
+
+    for r in results:
+        try:
+            model_q = Game.objects.get(filename=r['slug'])
+        except Game.DoesNotExist:
+            model_q = None
+
+        if model_q:
+            r['idb'] = 1
+        else:
+            r['idb'] = 0
+
+    if not os.path.isdir(lutris_dir):
+        os.makedirs(lutris_dir)
+
+    with open(lutris_json, 'w') as f:
+        json.dump(results, f, indent=4)
