dda6b06a4bfbfaa527af98e5c3e3d7d7130c6a1c
xerocuil <xerocuil@mm.st>
2024-07-06 00:13:53 -0400

    Added API urls to `gamedex.urls`

--- gamedex/urls.py
+++ gamedex/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', include('library.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.API_URL, document_root=settings.API_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
